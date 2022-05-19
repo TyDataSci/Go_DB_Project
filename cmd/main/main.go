f1bccd8afac552fcf73378f0a2fcf5274bff7575
Ty Sanders <tysanders@tys-macbook-air.lan>
2022-05-19 09:54:48 -0500

    Config  pass user table

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/TyDataSci/Go_DB_Project/pkg/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
